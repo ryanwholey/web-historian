c95d8e4e972604fee235ba8d2536f5e0162f63c1
ryanwholey <rjwholey@gmail.com>
2015-09-09 17:00:02 -0700

    added helper functions, scrapes html, redirects to index.html

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var request = require('request');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -22,20 +23,75 @@ exports.initialize = function(pathsObj) {
   });
 };
 
+exports.realPath = function(path){
+  return path.join(__dirname,'./',path);
+}
+
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function() {
+exports.readListOfUrls = function(cb) {
+  fs.readFile(path.join(__dirname,'./','../archives/sites.txt'),"utf-8", function(err, data){
+    if(err){throw err}
+    cb(data.split('\n'));
+  });
 };
 
-exports.isUrlInList = function() {
+exports.isUrlInList = function(url, cb) {
+  exports.readListOfUrls(function(d){
+    cb(_.contains(d, url));
+  });
 };
 
-exports.addUrlToList = function() {
+
+exports.addUrlToList = function(url) {
+  exports.isUrlInList(url, function(contains){
+    if(!contains){
+      fs.appendFile(path.join(__dirname,'./','../archives/sites.txt'),url+'\n');
+    }
+  });
 };
 
-exports.isUrlArchived = function() {
+exports.isUrlArchived = function(url, cb) {
+  fs.exists(path.join(__dirname,'./','../archives/sites/'+ url +'.html'), function(exists){ cb(exists); });
 };
+
+
 
 exports.downloadUrls = function() {
+  exports.readListOfUrls(function(arr){
+    _.each(arr, function(url){
+      exports.isUrlArchived(url, function(archived){
+        if(!archived){
+          newUrl = 'http://' + url;
+          request(newUrl, function(err, response, body) {
+            if (!err && response.statusCode === 200) {
+              fs.writeFile(path.join(__dirname,'./','../archives/sites/' + url + '.html'), body, function(err){
+                if(err){ throw err; }
+              });
+            }
+          })
+        }
+      });
+    });
+  });
 };
+
+
+
+
+  // exports.isUrlArchived(url, function(exists){
+  //   if(exists){
+  //     request(url, function(err, response, body) {
+  //       if (!err && response.statusCode === 200) {
+  //         console.log(body);
+  //       }
+  //     })
+  //   } else{
+  //     console.log('isn"t there');
+  //   }
+  // })
+// };
+
+exports.downloadUrls();
+
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,53 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+
+
 // require more modules/folders here!
 
+var index = fs.readFileSync(path.join(__dirname, './', 'public/index.html'));
+var loading = fs.readFileSync(path.join(__dirname, './', 'public/loading.html'));
+var styles = fs.readFileSync(path.join(__dirname, './', 'public/styles.css'));
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if(req.url ==='/'){
+    res.writeHeader(200, {"Content-Type" : "text/html"});
+    res.end(index);
+  }
+
+  if(req.method === 'POST'){
+    res.writeHeader(201, {"Content-Type" : "text/html"});
+    var dataString = '';
+
+    req.on('data', function(chunk){
+      console.log('chunk',chunk);
+      dataString += chunk;
+    });
+    req.on('end', function(){
+      console.log(typeof dataString)
+      dataString = dataString.substring(4);
+      console.log(dataString);
+
+      if(dataString === ''){
+        res.end();  
+        return;
+      }
+      
+
+
+
+
+    //   fs.open(path.join(__dirname,'./','../archives/sites.txt'), 'a', function(err, fd){
+    //     fs.write(fd, dataString + "\n");
+    //     fs.close(fd);
+    //   });
+
+
+    });
+    res.end(loading);
+  }
+
+
+
+  res.end();
 };
